d59e70bfa784635892c4b3dfb77bbed6651e3c61
samrosemohammed <samrose.mohammed@gmail.com>
2025-11-22 11:34:39 +0545

    integrated channel messaging

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/message/message-item.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/message/message-item.tsx
@@ -1,25 +1,18 @@
 import Image from "next/image";
 import React from "react";
 import { format } from "date-fns";
-
+import { Message } from "@/app/generated/prisma/client";
+import { getAvatar } from "@/lib/get-avatar";
+import { SaveContent } from "@/components/raich-text-editor/save-content";
 interface iAppProps {
-  id: number;
-  message: string;
-  date: Date;
-  avatar: string;
-  userName: string;
+  message: Message;
 }
-export const MessageItem = ({
-  id,
-  message,
-  date,
-  avatar,
-  userName,
-}: iAppProps) => {
+export const MessageItem = ({ message }: iAppProps) => {
+  console.log("Rendering message:", message);
   return (
     <div className="flex space-x-3 relative p-3 rounded-lg group hover:bg-muted/50">
       <Image
-        src={avatar}
+        src={getAvatar(message?.authorAvatar, message?.authorEmail)}
         alt="User Avatar"
         width={32}
         height={32}
@@ -27,12 +20,15 @@ export const MessageItem = ({
       />
       <div className="flex-1 space-y-1 min-w-0">
         <div className="flex items-center gap-x-2">
-          <p className="font-medium leading-none">{userName}</p>
+          <p className="font-medium leading-none">{message.authorName}</p>
           <p className="text-xs text-muted-foreground leading-none">
-            {format(date, "d MMM yyyy h:mm a")}
+            {format(message.createdAt, "d MMM yyyy h:mm a")}
           </p>
         </div>
-        <p className="text-sm break-all max-w-none">{message}</p>
+        <SaveContent
+          className="break-word text-sm prose dark:prose-invert marker:text-primary max-w-none"
+          content={JSON.parse(message.content)}
+        />
       </div>
     </div>
   );
--- app/router/message.ts
+++ app/router/message.ts
@@ -8,6 +8,7 @@ import prisma from "@/lib/db";
 import { createMessageSchema } from "@/schemas/message";
 import { getAvatar } from "@/lib/get-avatar";
 import { Message } from "../generated/prisma/client";
+import { readSecurityMiddleware } from "../middlewares/arcject/read";
 
 export const createMessage = base
   .use(requireAuthMiddleware)
@@ -40,7 +41,7 @@ export const createMessage = base
         imageUrl: input.imageUrl,
         channelId: input.channelId,
         authorId: context.user.id,
-        authorAvatar: getAvatar(context.user.email!, context.user.given_name!),
+        authorAvatar: getAvatar(context.user.picture, context.user.email!),
         authorName: context.user.given_name ?? "John Doe",
         authorEmail: context.user.email!,
       },
@@ -48,4 +49,42 @@ export const createMessage = base
     return {
       ...created,
     };
+  });
+
+export const listMessages = base
+  .use(requireAuthMiddleware)
+  .use(requireWorkspacehMiddleware)
+  .use(standardSecurityMiddleware)
+  .use(readSecurityMiddleware)
+  .route({
+    method: "GET",
+    path: "/messages",
+    summary: "List all messages",
+    tags: ["Messages"],
+  })
+  .input(
+    z.object({
+      channelId: z.string(),
+    })
+  )
+  .output(z.array(z.custom<Message>()))
+  .handler(async ({ input, context, errors }) => {
+    const channel = await prisma.channel.findFirst({
+      where: {
+        id: input.channelId,
+        workspaceId: context.workspace.orgCode,
+      },
+    });
+    if (!channel) {
+      throw errors.FORBIDDEN();
+    }
+    const data = await prisma.message.findMany({
+      where: {
+        channelId: input.channelId,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return data;
   });
